d6a17834acc8766cd02266ac89f7143153200b90
Anisio <anisiomandlate@gmail.com>
2022-02-06 18:21:23 +0530

    feat: Add Login Page

---
+++ src/components/Auth/Login/index.jsx
@@ -0,0 +1,41 @@
+import { Link } from "react-router-dom";
+
+// Styles
+import styles from "./Login.module.css";
+
+const Login = () => {
+  return (
+    <div className={styles["form-container"]}>
+      <h1>
+        Account <span className={styles["text-primary"]}>Login</span>
+      </h1>
+      <form className={styles.form}>
+        <div className={styles["form-group"]}>
+          <label htmlFor="email">Email</label>
+          <input
+            type="email"
+            name="email"
+            // value={email}
+          />
+        </div>
+
+        <div className={styles["form-group"]}>
+          <label htmlFor="password">Password</label>
+          <input
+            type="password"
+            name="password"
+            // value={password}
+            // onChange={onChange}
+          />
+        </div>
+        <input
+          type="submit"
+          value="Login"
+          className={styles["submit-button"]}
+        />
+      </form>
+    </div>
+  );
+};
+
+export default Login;
