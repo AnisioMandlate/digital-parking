8fda2b035679bde541215d92efaa7b658da06682
Anisio <anisiomandlate@gmail.com>
2022-02-06 19:22:29 +0530

    feat: Add header for homepage'

--- src/components/pages/Home/index.jsx
+++ src/components/pages/Home/index.jsx
@@ -1,15 +1,30 @@
 import { Link } from "react-router-dom";
 
+// Styles
+import styles from "./Home.module.css";
+
 const Home = () => {
   return (
     <div>
-      <h1> This is the home page</h1>
-      <p>
-        <Link to="/login">Go to Login</Link>
-      </p>
-      <p>
-        <Link to="/about">Go to About</Link>
-      </p>
+      <header className={styles.header}>
+        <nav>
+          <ul>
+            <li>Hi, User!</li>
+            <button className={styles["header-button"]}>
+              <i className="fas fa-sign-out-alt"></i> Logout
+            </button>
+          </ul>
+        </nav>
+      </header>
+      <div className={styles.container}>
+        <h1>Welcome to Digital Parking system</h1>
+        <p>
+          <Link to="/login">Go to Login</Link>
+        </p>
+        <p>
+          <Link to="/about">Go to About</Link>
+        </p>
+      </div>
     </div>
   );
 };
